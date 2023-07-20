41aa8c8eba23b1eefd1978d22017e7db5f801d87
leandro barel <leandrobarel@gmail.com>
2023-07-19 21:44:00 -0300

    ultima alteraçao

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def view_home(request):
+def views_home(request):
     return render(request, 'paginas/index_home.html')
--- inicio/urls.py
+++ inicio/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('terrestre/', include('terrestre.urls')),
+    path('aquaticos/', include('aquaticos.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
