ad90b6ac6aa8094b8452f0d0e4348cfc5d078e22
cristofer <cristoferfontela72@gmail.com>
2023-07-20 21:32:09 -0300

    atualizando-aquaticos

--- home/urls.py
+++ home/urls.py
@@ -4,9 +4,5 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', views.views_home, name='index'),
-=======
-    path('', views.views_home, name='home'),
->>>>>>> 41aa8c8eba23b1eefd1978d22017e7db5f801d87
 ]
